3b013cabe7579c216e8686f5c028a63f7aa65fcc
Guest User <Guest@Epicodus-5D.local>
2017-08-02 16:27:11 -0700

    add another spec of function

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -15,18 +15,30 @@ public class App {
             Hangman hangman = new Hangman();
             hangman.pickFirstWord();
             hangman.getDashed();
+            System.out.println("Select difficulty: easy, medium, hard");
+            int tries = 0;
+            String turns = bufferedReader.readLine().toLowerCase();
+            if(turns == "hard"){
+               return tries += 5;
+            }else if(turns == "medium"){
+                tries += 10;
+            }else if(turns == "easy"){
+                tries += 15;
+            }
             System.out.println(hangman.getDashedWord());
+            System.out.println("How many Guesses Left: "+(tries));
             System.out.println("Let's play Hangman, please enter a letter.");
             String usedletters = bufferedReader.readLine().toLowerCase();
             hangman.searchLetter(usedletters);
             System.out.println(hangman.getDashedWord());
+
             int guess= 1;
 
             while (gameRunning){
-              if(hangman.solvedGame(hangman.getDashedWord())){
+                if(hangman.solvedGame(hangman.getDashedWord())){
                   System.out.println("Congratulations, you've solved it. The word is "+ hangman.getDashedWord()+"!");
                   gameRunning = false;
-              }else if (guess == 10){
+              }else if (guess == tries){
                   System.out.println("Your guessing skills are hot garbage!");
                   gameRunning = false;
               }else{
