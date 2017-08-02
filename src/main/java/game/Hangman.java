4671a94c8de56a6af83e79028c82dbdabfbd52a8
Guest User <Guest@Epicodus-5D.local>
2017-08-02 09:23:13 -0700

    add test cases

---
+++ src/main/java/game/Hangman.java
@@ -0,0 +1,7 @@
+package game;
+
+/**
+ * Created by Guest on 8/2/17.
+ */
+public class hangman {
+}
---
+++ src/test/java/game/HangmanTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Guest on 8/2/17.
+ */
+public class hangmanTest {
+
+}
